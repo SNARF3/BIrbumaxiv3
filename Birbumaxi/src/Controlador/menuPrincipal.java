7076befdd425dafcd0aa3357fa85a870858113f4
SNARF3 <snarfeo@gmail.com>
2024-11-16 20:32:25 -0400

    daeda

--- Birbumaxi/src/Controlador/menuPrincipal.java
+++ Birbumaxi/src/Controlador/menuPrincipal.java
@@ -7,11 +7,10 @@ import java.math.RoundingMode;
 import Modelo.EnviarEmailFactura;
 import Modelo.FacturaEnPDF;
 import Vista.Factura;
-<<<<<<< HEAD
 import Vista.Reportes;
-=======
+
 import Vista.MenuGerente;
->>>>>>> bd183f97243b07818663bfafe7af8cd46b353c2e
+
 import Vista.Ventas;
 
 //import Vista.MenuGerente;
@@ -21,17 +20,16 @@ import Vista.login;
 public class menuPrincipal {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		//login ventana1 = new login();
 		//ventana1.setVisible(true);
 		/*MenuGerente menug = new MenuGerente();
 		menug.setVisible(true);*/
-=======
+
 		/*login ventana1 = new login();
 		ventana1.setVisible(true);*/
 		MenuGerente menug = new MenuGerente();
 		menug.setVisible(true);
->>>>>>> bd183f97243b07818663bfafe7af8cd46b353c2e
+
 		//MenuGerente menug = new MenuGerente();
 		//menug.setVisible(true);
 		//Ventas v = new Ventas();
